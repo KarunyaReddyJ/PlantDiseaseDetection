0351abcf6acd2ceae3369184ad0940ec5b75539c
KarunyaReddyJ <vpk8908@gmail.com>
2024-11-29 03:06:58 +0530

    i

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, render_template, jsonify, url_for
 from tensorflow.keras.models import load_model
+import requests
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 import os
 
 app = Flask(__name__)
 
+def load_model_from_url(url, local_path):
+    if not os.path.exists(local_path):
+        print("Downloading model...")
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(local_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        else:
+            raise Exception(f"Failed to download file. Status code: {response.status_code}")
+    return load_model(local_path)
+
+# Use the modified Google Drive link
+model_url = "https://drive.google.com/uc?id=1z9kbfDnT64-TzM6Dx-IUjUcuY91cXk3L&export=download"
+model_path = "model.h5"
+
+model = load_model_from_url(model_url, model_path)
 # Load your trained model
-model = load_model('trained_plant_disease_model.keras')
+#model = load_model('trained_plant_disease_model.keras')
 
 def model_predict(img_path, model):
     # Load the image with the same size as the training images
